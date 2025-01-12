99caf4f57f66f30024c5bf3ae8ff6bb4b759c6fd
lock94656 <gibert.hug@gmail.com>
2025-01-12 18:43:56 +0100

    First working version with bot playing random

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main()
 	int tab[9] = {0,0,0,0,0,0,0,0,0};
 	int nbCase;
 	
+	std::cout<<std::endl;
 	std::cout<<"Tic Tac Toe"<<std::endl;
 	std::cout<<"-----------"<<std::endl;
 	std::cout<<"You are the X"<<std::endl;
@@ -17,31 +18,45 @@ int main()
 	
 	printBoard(tab, 9);
 	
-	while (lookForWinner(tab)!=0 || isAllCaseTaken(tab)!=1)
+	while (1) //lookForWinner(tab) == 0 || isAllCaseTaken(tab) != 1
 	{
 		do
 		{
 			std::cout<<"Your turn. Enter a number (0 to 8). ";
 			std::cin>>nbCase;
 			std::cout<<std::endl;
-		}
-		while (isCaseTaken(tab, nbCase) != 0);
+		}while (isCaseTaken(tab, nbCase) != 0);
+		
+		
 		tab[nbCase] = 1;
 		printBoard(tab, 9);
 		
+		if(lookForWinner(tab) == 1)
+		{
+			std::cout<<"You win !!!"<<std::endl;
+			return 0;
+		}
+		if (isAllCaseTaken(tab) == 1)
+		{
+			std::cout<<"Equality"<<std::endl;
+			return 0;
+		}
 		std::cout<<"Bot's turn."<<std::endl;
 		int random = 0;
+	
 		do 
 		{
-			
 			random = rand() % 9;
-			
-		}
-		while (isCaseTakenBot(tab, random) != 0);
+		}while (isCaseTakenBot(tab, random) != 0);
 		
 		tab[random] = 2;
 		printBoard(tab, 9);
+		
+		if(lookForWinner(tab) == 2)
+		{
+			std::cout<<"You lose !!!"<<std::endl;
+			return 0;
+		}
 	}
-	
-	return 0;
+	return 1;
 }
